8affdaa0f68aa287277618e3eb2f3f0cb0bea264
Chandler M. Armstrong <cmarmstrong@cerrrb-wn-t1396.erdc.dren.mil>
2019-01-27 11:50:21 -0500

    correct argument passing to rnaturalearth::ne_countries

--- R/v.R
+++ R/v.R
@@ -16,33 +16,36 @@
 #'   higher resolution vector.  Default TRUE.
 #' @param resolution  The resolution if cb is TRUE.  Applies only to FIPS.
 #'   Options are 500k, 5m, and 20m for 1:500k, 1:5 million, and 1:20 million.
+#' @param buffer Logical.  If true will buffer the vector data to the value of
+#'   the argument `width'.  Will not work with `returnclass' set to 'sf'.
 #' @param ... Additional arguments passed to
 #'   \code{\link[rnaturalearth]{ne_states}},
 #'   \code{\link[rnaturalearth]{ne_countries}}, or
 #'   \code{\link[tigris]{counties}}.  Use `returnclass' (country and hasc) or
-#'   `class' (fips) to set the class of the returned geometry.  Both arguments
-#'   accept a value of either `sf' or `sp'.
+#'   to set the class of the returned geometry.  Accepts a value of either `sf'
+#'   or `sp'.
 #' @return A vector for the administrative boundary identified by the location
 #'   code
 #' @export
-v <- function(country=NULL, fips=NULL, hasc=NULL, cb=TRUE, resolution='20m', ...) {
+v <- function(country=NULL, hasc=NULL, fips=NULL, cb=TRUE, resolution='20m', buffer=FALSE,
+              returnclass='sp', ...) {
     if(sum(!sapply(formals()[1:3], is.null))>1) stop('Supply only one of country, fips, or hasc')
     v <- NULL
     if(!is.null(hasc)) {
         lHasc <- strsplit(hasc, '\\.')
         country <- sapply(lHasc, '[', 1)
         country <- countrycode::countrycode(country, 'iso2c', 'country.name')
-        states <- rnaturalearth::ne_states(country=country, ...)
+        states <- rnaturalearth::ne_states(country=country, returnclass=returnclass)
         v <- states[states $code_hasc %in% hasc, ]
     } else if(!is.null(country)) {
-        v <- rnaturalearth::ne_countries(country=country, ...)
+        v <- rnaturalearth::ne_countries(country=country, returnclass=returnclass)
     }
     if(!is.null(fips)) {
         state <- substr(fips, 1, 2)
         county <- substr(fips, 3, 5)
-        counties <- tigris::counties(state=unique(state), cb, resolution, ...)
+        counties <- tigris::counties(state=unique(state), cb, resolution, class=returnclass)
         v <- counties[counties $GEOID %in% fips, ]
     }
+    if(buffer) v <- rgeos::gBuffer(v, width)
     v
 }
-

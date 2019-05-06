1303c24d2b9eb9ed3a528932694984d32f1f31fb
Chandler M. Armstrong <cmarmstrong@cerrrb-wn-t1396.erdc.dren.mil>
2019-05-06 10:18:56 -0400

    add scale parameter to v for rnaturalearth

--- R/v.R
+++ R/v.R
@@ -3,7 +3,8 @@
 #' Accepts a place code and returns an administrative boundary vector.  This
 #' function will service place codes from rnaturalearth place names, Federal
 #' Information Processing Standard (FIPS) codes, and hierarchical administrative
-#' subdivision codes (HASC).
+#' subdivision codes (HASC).  If no names or codes are given, returns an
+#' \code{\link[raster]{extent}} for the globe in longitude/latitude.
 #'
 #' Vector data is downloaded from rnaturalearth (place names and HASC) or tigris
 #' (FIPS codes)
@@ -14,12 +15,15 @@
 #' @param cb Logical.  Applies only to FIPS codes.  If true will return a
 #'   generalized resolution cartographic boundary vector, otherwise returns a
 #'   higher resolution vector.  Default TRUE.
-#' @param resolution  The resolution if cb is TRUE.  Applies only to FIPS.
-#'   Options are 500k, 5m, and 20m for 1:500k, 1:5 million, and 1:20 million.
+#' @param resolution  The resolution if cb is TRUE.  Applies only with argument
+#'   `FIPS'.  Options are 500k, 5m, and 20m for 1:500k, 1:5 million, and 1:20
+#'   million.
 #' @param buffer Integer.  If greater than zero will buffer the vector data to
 #'   the value of this argument.  Will not work with `returnclass' set to 'sf'.
 #' @param returnclass set the class of the returned geometry.  Accepts a value
 #'   of either `sf' or `sp'.
+#' @param scale The scale of the vector data.  Applies only with arguments
+#'   `country' or `hasc'.  Options are 'small', 'medium', or 'large'.
 #' @param ... Additional arguments, unused.
 #' @return A vector for the administrative boundary identified by the location
 #'   code
@@ -28,7 +32,7 @@
 #'   \code{\link[countrycode]{countrycode}}
 #' @export
 v <- function(country=NULL, hasc=NULL, fips=NULL, cb=TRUE, resolution='20m', buffer=0,
-              returnclass='sp', ...) {
+              returnclass='sp', scale='large', ...) {
     ## if(sum(!sapply(formals()[1:3], is.null))>1) stop('Supply only one of country, fips, or hasc')
     if(!is.null(fips)) {
         if(!is.null(country) && country=='United States of America') {
@@ -44,11 +48,11 @@ v <- function(country=NULL, hasc=NULL, fips=NULL, cb=TRUE, resolution='20m', buf
         lHasc <- strsplit(hasc, '\\.')
         country <- sapply(lHasc, '[', 1)
         country <- countrycode::countrycode(country, 'iso2c', 'country.name')
-        states <- rnaturalearth::ne_states(country=country, returnclass=returnclass)
+        states <- rnaturalearth::ne_states(country=country, returnclass=returnclass, scale=scale)
         v <- states[states $code_hasc %in% hasc, ]
     } else if(!is.null(country)) {
-        v <- rnaturalearth::ne_countries(country=country, returnclass=returnclass)
-    }
-    if(buffer) v <- rgeos::gBuffer(v, buffer)
+        v <- rnaturalearth::ne_countries(country=country, returnclass=returnclass, scale=scale)
+    } else v <- raster::extent(c(-180, 180, -90, 90))
+    if(buffer) v <- rgeos::gBuffer(v, width=buffer)
     v
 }
